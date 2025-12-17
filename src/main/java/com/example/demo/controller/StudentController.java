3edc7869a681318635edf75444282554e6eeb4be
bala-amypo <bala@amypo.in>
2025-12-17 06:46:47 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,8 +1,6 @@
 package com.example.demo.controller;
-
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,22 +10,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class StudentController {
-
     @Autowired
     StudentService studentService;
-
     @PostMapping("/PostStudent")
     public Student postStd(@RequestBody Student st) {
         return studentService.insertStudent(st);
     }
-
     @GetMapping("/getAll")
     public List<Student> getAll() {
         return studentService.getAllStudents();
